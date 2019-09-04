0937bf5ee8adca531c0f79f5c436911cf9bb89c9
yuyaebean <yuyaebean@gmail.com>
2019-09-04 10:43:44 +0900

    Setup GraphQL server

---
+++ src/api/Test/sayTest/sayTest.js
@@ -0,0 +1,5 @@
+export default {
+  Query: {
+    sayTest: () => 'Test'
+  }
+};
--- src/server.js
+++ src/server.js
@@ -1,1 +1,15 @@
-console.log('hello!');
+import dotenv from 'dotenv';
+import { GraphQLServer } from 'graphql-yoga';
+import logger from 'morgan';
+import schema from './schema';
+
+dotenv.config();
+const PORT = process.env.PORT || 4000;
+
+const server = new GraphQLServer({ schema });
+
+server.express.use(logger('dev'));
+
+server.start({ port: PORT }, () => {
+  console.log(`Server is now running on http://localhost:${PORT}`);
+});
